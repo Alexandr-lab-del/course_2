571bf9f71e3cda01d5cae430f371ecd7744da8f0
Alexandr-lab-del <gordon675843@mail.ru>
2024-06-29 13:37:08 +0300

    add_homework_5

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.widget import get_data
+from src.widget import get_data, mask_account_card
 
 
 @pytest.mark.parametrize(
@@ -14,15 +14,25 @@ from src.widget import get_data
 def test_mask_account_card(input_data: str, expected_output: str) -> None:
     """Тест правильности работы функции mask_account_card
     на конкретных входных данных и ожидаемом выводе"""
+    assert mask_account_card(input_data) == expected_output
 
 
 @pytest.mark.parametrize(
     "input_data, expected_output",
     [
         ("2018-07-11T02:26:18.671407", "11.07.2018"),
+        ("2019-07-11T", "11.07.2019"),
+        ("2017-07-11T02:26:18.671407", "11.07.2017"),
+        ("2016-07-11T02:26:18.671407", "11.07.2016"),
     ],
 )
 def test_get_data(input_data: str, expected_output: str) -> None:
     """функция для проверки правильности работы функции get_data
     на конкретных входных данных и ожидаемом выводе"""
     assert get_data(input_data) == expected_output
+
+
+def test_get_data_error():
+    with pytest.raises(ValueError) as exc_info:
+        get_data(" ")
+        assert str(exc_info.value) == "Ошибка, введите значение"
